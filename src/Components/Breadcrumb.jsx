b0e3487c24f591421ef94d71e8ca1a3fb7dffa58
Fachifachi <gist140920@gmail.com>
2024-05-13 00:03:51 -0300

    arregle errores del breadcrumb

--- src/Components/Breadcrumb.jsx
+++ src/Components/Breadcrumb.jsx
@@ -9,17 +9,17 @@ const Breadcrumb = () => {
   useEffect(() => {
     const fetchMovieName = async () => {
       const segments = location.split('/');
-      if (segments.length >= 3 && segments[1] === 'movies') {
+      if (segments.length >= 3 && segments[1].toLowerCase() === 'movies') {
         const movieId = segments[2];
         try {
           const response = await axios.get(`https://ghibliapi.vercel.app/films/${movieId}`);
           setMovieName(response.data.title);
         } catch (error) {
           console.error('Error fetching movie details:', error);
-          setMovieName(null); // Si hay un error, establece el nombre de la película como nulo
+          setMovieName(null);
         }
       } else {
-        setMovieName(null); // Si la URL no corresponde a una película, establece el nombre de la película como nulo
+        setMovieName(null);
       }
     };
 
@@ -33,15 +33,24 @@ const Breadcrumb = () => {
   const pathSegments = getPathSegments();
 
   return (
-    <div>
-      <Link href="/">Home</Link>
+    <div className="flex justify-center items-center text-gray-900 text-2xl font-bold py-4">
+      <Link href="/" className="hover:text-gray-800">
+        Home
+      </Link>
       {pathSegments.map((segment, index) => (
-        <span key={index}>
-          {' > '}
+        <span key={index} className="flex items-center mx-4">
+          <svg
+            className="w-6 h-6 fill-current text-gray-700"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+          </svg>
           {index === pathSegments.length - 1 ? (
-            <span>{movieName || segment}</span>
+            <span>{movieName || segment.charAt(0).toUpperCase() + segment.slice(1)}</span>
           ) : (
-            <Link href={`/${pathSegments.slice(0, index + 1).join('/')}`}>{segment}</Link>
+            <Link href={`/${pathSegments.slice(0, index + 1).join('/')}`} className="hover:text-gray-800">
+              {segment === 'movies' ? 'Movies' : segment}
+            </Link>
           )}
         </span>
       ))}
