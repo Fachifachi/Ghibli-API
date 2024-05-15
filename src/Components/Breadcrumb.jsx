a03eeb620d327adb91aab42dfaa54273cef6a52d
Fachifachi <gist140920@gmail.com>
2024-05-15 14:07:55 -0300

    final

--- src/Components/Breadcrumb.jsx
+++ src/Components/Breadcrumb.jsx
@@ -9,30 +9,34 @@ const Breadcrumb = () => {
   const location = useLocation()[0];
 
   return (
-    <div className="flex justify-center items-center text-gray-900 text-xl font-bold bg-sky-blue flex-wrap py-2">
-      <Link href="/" className="hover:text-gray-800 mr-2">
-        Home
-      </Link>
-      {(location === '/' || pathSegments.includes('movies') || movieName) && (
-        <span className="mx-2">/</span>
-      )}
-      {pathSegments.map((segment, index) => (
-        <span key={index} className="flex items-center">
-          {index !== 0 && (
+    <>
+      {location !== '/' && (  // Mostrar solo si no estamos en la página de inicio
+        <div className="flex justify-center items-center text-gray-900 text-xl font-bold bg-sky-blue flex-wrap py-2">
+          <Link href="/" className="hover:text-gray-800 mr-2">
+            Home
+          </Link>
+          {(location === '/' || pathSegments.includes('movies') || movieName) && (
             <span className="mx-2">/</span>
           )}
-          {index === pathSegments.length - 1 ? (
-            <span className="ml-1" style={{ backgroundColor: location === '/' ? 'yellow' : '#3182ce' }}>
-              {movieName || segment.charAt(0).toUpperCase() + segment.slice(1)}
+          {pathSegments.map((segment, index) => (
+            <span key={index} className="flex items-center">
+              {index !== 0 && (
+                <span className="mx-2">/</span>
+              )}
+              {index === pathSegments.length - 1 ? (
+                <span className="ml-1" style={{ backgroundColor: location === '/' ? 'yellow' : '#3182ce' }}>
+                  {movieName || segment.charAt(0).toUpperCase() + segment.slice(1)}
+                </span>
+              ) : (
+                <Link href={`/${pathSegments.slice(0, index + 1).join('/')}`} className="hover:text-gray-800 mx-2">
+                  {segment === 'movies' ? 'Movies' : segment}
+                </Link>
+              )}
             </span>
-          ) : (
-            <Link href={`/${pathSegments.slice(0, index + 1).join('/')}`} className="hover:text-gray-800 mx-2">
-              {segment === 'movies' ? 'Movies' : segment}
-            </Link>
-          )}
-        </span>
-      ))}
-    </div>
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
